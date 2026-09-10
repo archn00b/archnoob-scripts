54366210f671f3ce86e2c063f020b8f5ee004f3e
archn00b <archn00b2024@gmail.com>
2026-09-10 08:57:11 -0700

    made some changes

--- install-desktop/install_bg.sh
+++ install-desktop/install_bg.sh
@@ -10,27 +10,19 @@
 # Setting script PATH 
 installed_dir=$(dirname "$(readlink -f "$(basename "$(pwd)")")")
 
-# ADDING ARCHN00B CORE-REPO TO /ETC/PACMAN.CONF
-sudo pacman -Syu starship alacritty
+sudo pacman -Syu
 
-# INSTALL ALACRITTY TERMINAL WITH THEME
-mkdir -p "$HOME/.config/alacritty/"
-sudo cp -r /etc/skel/.config/alacritty/alacritty.toml "$HOME/.config/alacritty/" 2>/dev/null
 
-# INSTALLING STARSHIP
-echo "Installing Starship." 
-mkdir -p "$HOME/.config/"
-sudo cp -r /etc/skel/.config/starship.toml "$HOME/.config/"
+# SETTING DEFAULT BACKGROUND
+git clone https://github.com/archn00b/wallpapers.git
+rm -rf wallpapers/.git wallpapers/pushit2git.sh
+sudo cp -r wallpapers/* /usr/share/backgrounds/xfce/
+rm -rf wallpapers
 
-if [ -f ~/.bashrc ]; then
-    cp ~/.bashrc ~/.bashrc.bak."$(date +"%Y%m%d_%H%M%S")"
-    echo 'eval "$(starship init bash)"' >> ~/.bashrc
-    source ~/.bashrc
-fi
-
+# COMMAND TO SET BACKGROUND
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/None-1/workspace0/last-image -s /usr/share/backgrounds/xfce/bg3.jpg
 
 echo "##########################################"
 echo "##### INSTALLATION DONE, REBOOTING... #####"
 echo "##########################################"
 sleep 5
-reboot
