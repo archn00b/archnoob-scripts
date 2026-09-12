001d309e510e63ae4d1fbbd5b5e54c78eb8a6982
archn00b <archn00b2024@gmail.com>
2026-09-12 02:34:23 +0000

    lets try this

--- change-wallpaper/change-wallpaper.sh
+++ change-wallpaper/change-wallpaper.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-git clone https://github.com/archn00b/wallpapers.git
-rm -rf wallpapers/.git
-rm -rf wallpapers/pushit2git.sh
-sudo cp -rf wallpapers/* /usr/share/backgrounds/xfce/
-rm -rf wallpapers
-property=/backdrop/screen0/monitorDP-4/workspace0/last-image
-bg=/usr/share/backgrounds/xfce/bg3.jpg
 
-xfconf-query -c xfce4-desktop -p $property -s $bg
+property="/backdrop/screen0/monitorDP-1/workspace0/last-image"
+bg="/usr/share/backgrounds/xfce/bg3.jpg"
+cp_destination="/usr/share/backgrounds/xfce/"
+
+change_wallpaper(){
+    git clone https://github.com/archn00b/wallpapers.git
+    sudo cp -rf wallpapers/* "$cp_destination"
+    rm -rf wallpapers
+
+    xfconf-query -c xfce4-desktop -p "$property" -s "$bg"
+}
+
+main(){
+  change_wallpaper
+}
+
+main
